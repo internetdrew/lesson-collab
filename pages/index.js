9c1c16efa59fd9ecbe68f702fc7b5392d6d9fab2
internetdrew <andrew@internetdrew.com>
2023-03-28 00:38:02 -0400

    add footer and navbar to home

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { Footer, Navbar } from '../components';
+
 export default function Home() {
   return (
     <>
+      <Navbar />
       <h1>hello world</h1>
+      <h2>This is home</h2>
+      <Footer />
     </>
   );
 }
