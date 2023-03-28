4d13284bccc37aa971f4b96bb8c312bf0090e317
internetdrew <andrew@internetdrew.com>
2023-03-28 13:58:22 -0400

    improve navigation styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { NavProfile } from '.';
+import { IoLogOutOutline } from 'react-icons/io5';
 
 const Navbar = () => {
   const subjects = [
@@ -11,26 +12,33 @@ const Navbar = () => {
   ].sort();
 
   return (
-    <nav className='h-screen flex flex-col p-4 bg-slate-100'>
-      <h1 className='text-center font-semibold text-2xl my-4'>
+    <nav className='h-screen flex flex-col px-6 bg-slate-100'>
+      <h1 className='text-center font-semibold text-2xl my-8'>
         <Link href={'/'}>LessonFeed</Link>
       </h1>
       <NavProfile />
-      <ul className='flex flex-col gap-6 text-lg font-medium ml-1'>
-        <li>
+      <ul className='flex flex-col  text-lg font-medium'>
+        <li className='pl-6 py-4 rounded-lg duration-300 ease-out hover:bg-white hover:shadow-md'>
           <Link href='/'>Home</Link>
         </li>
         {subjects.map(subject => (
-          <li key={subject} className='capitalize '>
-            <Link href={`/subject/${subject.split(' ').join('-')}`}>
+          <li
+            key={subject}
+            className='capitalize py-4 pl-6 rounded-lg duration-300 ease-in-out hover:bg-white hover:shadow-md'
+          >
+            <Link
+              href={`/subject/${subject.split(' ').join('-')}`}
+              className='w-full block'
+            >
               {subject}
             </Link>
           </li>
         ))}
       </ul>
-      <div className='mt-auto text-lg'>
+      <button className='mt-auto text-lg mb-10 flex items-center gap-4 text-red-600 font-semibold pl-6'>
+        <IoLogOutOutline className='text-2xl' />
         <span>Logout</span>
-      </div>
+      </button>
     </nav>
   );
 };
