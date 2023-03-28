fa4658309b1f18953084b3a6b7ad8c017a7307bb
internetdrew <andrew@internetdrew.com>
2023-03-28 10:51:59 -0400

    implement src directory

--- src/components/NavProfile.jsx
+++ src/components/NavProfile.jsx
@@ -2,8 +2,8 @@ import { AiOutlineUser } from 'react-icons/ai';
 
 const NavProfile = () => {
   return (
-    <div className='w-full flex items-center my-10 bg-white px-4 py-2 rounded-lg border shadow-lg'>
-      <div className='h-10 w-10 bg-blue-400 flex items-center justify-center rounded-full mr-4'>
+    <div className='flex items-center my-10 bg-white px-4 py-2 rounded-lg border hover:shadow-lg duration-300 cursor-pointer'>
+      <div className=' h-10 w-10 bg-blue-400 flex items-center justify-center rounded-full mr-4'>
         <AiOutlineUser className='text-2xl' />
       </div>
       <span className='text-lg font-semibold'>Test User</span>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import { NavProfile } from '.';
+
+const Navbar = () => {
+  const subjects = ['math', 'science', 'social studies', 'art', 'history'];
+
+  return (
+    <nav className='h-screen flex flex-col p-4 bg-slate-100'>
+      <NavProfile />
+      <ul className='flex flex-col gap-4'>
+        {subjects.map(subject => (
+          <Link
+            key={subject}
+            href={`/${subject}`}
+            className='text-lg capitalize border-b pb-2 border-slate-200'
+          >
+            {subject}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
