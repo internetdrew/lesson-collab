27043c2518f2015c49a77d7603f41328de0987f5
internetdrew <andrew@internetdrew.com>
2023-03-28 00:42:51 -0400

    add layout for later usage

---
+++ components/Layout.jsx
@@ -0,0 +1,13 @@
+import { Footer, Navbar } from '../components';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
