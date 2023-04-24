21c00b5f253bbfbe1dee6acfea3debd1e39ad555
internetdrew <andrew@internetdrew.com>
2023-04-24 16:17:49 -0400

    make post fetch dynamic for query params to endpoint

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,12 +13,12 @@ export default function Home({ posts }) {
 
 export const getServerSideProps = async ({ req, res, query }) => {
   const { subject } = query;
-  console.log(subject);
   const response = await axios.get(
-    `${process.env.SITE_URL}/api/posts?subject=${subject}`
+    subject
+      ? `${process.env.SITE_URL}/api/posts?subject=${subject}`
+      : `${process.env.SITE_URL}/api/posts`
   );
   const posts = response.data;
-  console.log(posts);
 
   return { props: { posts } };
 };
