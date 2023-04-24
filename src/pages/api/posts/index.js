b0429804e6b64e22727538c303d76b2f613d5eac
internetdrew <andrew@internetdrew.com>
2023-04-24 14:13:41 -0400

    add query for fetching posts

--- src/pages/api/posts/index.js
+++ src/pages/api/posts/index.js
@@ -1,3 +1,5 @@
+import { db } from '@/src/db/db';
+
 export default function handler(req, res) {
   const { method } = req;
 
@@ -6,6 +8,12 @@ export default function handler(req, res) {
       const query = req.query.subject
         ? 'SELECT * FROM posts WHERE subject = ?'
         : 'SELECT * FROM posts';
+
+      db.query(query, [req.query.subject], (err, data) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json(data);
+      });
       break;
 
     case 'POST':
