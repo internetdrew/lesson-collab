b68bf15d21051b40a84a4a20390944d94b4b227a
internetdrew <andrew@internetdrew.com>
2023-04-27 11:20:49 -0400

    make post data dynamic based on edit id

--- src/pages/create/index.jsx
+++ src/pages/create/index.jsx
@@ -2,14 +2,14 @@ import { Navbar, NewPostForm } from '@/src/components';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../api/auth/[...nextauth]';
 
-const Create = () => {
+const Create = ({ postData }) => {
   return (
     <div>
       <Navbar />
       <main className='mt-24'>
         <div className='mx-auto max-w-screen-md px-4 pb-6 sm:px-6 lg:px-8 lg:pb-16'>
           <div className='bg-white rounded-lg'>
-            <NewPostForm />
+            <NewPostForm postData={postData} />
           </div>
         </div>
       </main>
@@ -21,6 +21,10 @@ export default Create;
 
 export const getServerSideProps = async ctx => {
   const session = await getServerSession(ctx.req, ctx.res, authOptions);
+  const { edit: postId } = ctx.query;
+
+  const res = await fetch(`${process.env.SITE_URL}/api/posts/${postId}`);
+  const postData = postId ? await res.json() : null;
 
   // if (!session) {
   //   return {
@@ -30,8 +34,7 @@ export const getServerSideProps = async ctx => {
   //     },
   //   };
   // }
-
   return {
-    props: { session },
+    props: { postData },
   };
 };
