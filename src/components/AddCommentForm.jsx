b266730aea98ccb0327e26a724a48eed650e2f7b
internetdrew <andrew@internetdrew.com>
2023-04-30 16:15:58 -0400

    remove prop drill for userId

--- src/components/AddCommentForm.jsx
+++ src/components/AddCommentForm.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 
-const AddCommentForm = ({ postId, userId }) => {
+const AddCommentForm = ({ postId }) => {
   const [commentText, setCommentText] = useState('');
   const router = useRouter();
 
--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -89,7 +89,7 @@ const PostDetails = ({ post }) => {
           </div>
         </div>
         <div className='px-4 py-4 sm:px-6'>
-          <AddCommentForm postId={post?.id} userId={currentUser?.id} />
+          <AddCommentForm postId={post?.id} />
           <p className='text-gray-500 mb-2'>Feedback</p>
           <Comment />
         </div>
