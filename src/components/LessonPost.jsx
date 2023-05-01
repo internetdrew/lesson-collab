50ae1b5a0980fa6cfd9dd6ec1c466c3dda9e0e70
internetdrew <andrew@internetdrew.com>
2023-05-01 18:08:07 -0400

    render out comment count on deadmount

--- src/components/LessonPost.jsx
+++ src/components/LessonPost.jsx
@@ -2,14 +2,20 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { AiOutlineComment } from 'react-icons/ai';
 import axios from 'axios';
+import Image from 'next/image';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { supabase } from '../db/db';
 
 const LessonPost = ({ post }) => {
   const [commentCount, setCommentCount] = useState(null);
-  const { id, title, desc, username } = post;
+  console.log(post);
 
   const fetchComments = async () => {
-    const res = await axios.get(`/api/comments/${id}`);
-    setCommentCount(res.data.length);
+    const { data } = await supabase
+      .from('comments')
+      .select()
+      .eq('post_id', post?.id);
+    setCommentCount(data.length);
   };
 
   useEffect(() => {
@@ -22,22 +28,27 @@ const LessonPost = ({ post }) => {
         <div className='px-4 sm:px-6'>
           {/* Content goes here */}
           {/* We use less vertical padding on card headers on desktop than on body sections */}
-          <h2 className='text-xl font-bold text-gray-900'>{title}</h2>
+          <h2 className='text-xl font-bold text-gray-900'>{post?.title}</h2>
         </div>
         <div className='px-4 py-2 sm:px-6 sm:py-3'>
           {/* Content goes here */}
-          <p>{desc}</p>
+          <p>{post?.desc}</p>
           <hr className='mt-4' />
         </div>
 
         <div className='px-4 flex items-center gap-2 text-gray-600 text-sm mt-1 sm:mt-0 sm:px-6'>
-          <span className='inline-flex shrink-0 h-8 w-8 items-center justify-center rounded-full bg-gray-500'>
-            <span className='font-medium leading-none text-white'>TU</span>
+          <span className='inline-flex overflow-hidden shrink-0 h-8 w-8 items-center justify-center rounded-full bg-gray-500'>
+            <Image
+              src={post?.profiles?.avatar}
+              alt='user image'
+              width={48}
+              height={48}
+            />
           </span>
           <p className='hidden sm:inline-block'>
             Posted by{' '}
             <span className='text-teal-600 duration-300 hover:text-teal-500'>
-              <Link href={`/profile/${username}`}>@{username}</Link>
+              <Link href={`/profile/${''}`}>{post?.profiles?.name}</Link>
             </span>
           </p>
           <div className='flex items-center ml-auto gap-4'>
@@ -49,7 +60,7 @@ const LessonPost = ({ post }) => {
             ) : null}
             <button className='my-2 px-3 py-1 bg-teal-600 text-white rounded duration-300 hover:shadow-lg hover:bg-teal-500'>
               <Link
-                href={`/posts/${id}`}
+                href={`/posts/${post?.id}`}
                 className='font-semibold flex items-center'
               >
                 {' '}
