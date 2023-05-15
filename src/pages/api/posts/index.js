0da10cfcf11c3c6f621b7c46905c345cf5de5d11
internetdrew <andrew@internetdrew.com>
2023-05-15 13:35:32 -0400

    change server client name

--- src/pages/api/posts/index.js
+++ src/pages/api/posts/index.js
@@ -3,7 +3,7 @@ import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
 export default async function handler(req, res) {
   const { method } = req;
   const { subject } = req.query;
-  const supabaseServerClient = createServerSupabaseClient({
+  const supabase = createServerSupabaseClient({
     req,
     res,
   });
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   switch (method) {
     case 'GET':
       if (subject) {
-        const { data } = await supabaseServerClient
+        const { data } = await supabase
           .from('posts')
           .select(
             `*, users (
@@ -22,8 +22,7 @@ export default async function handler(req, res) {
         return res.status(200).json(data);
       }
 
-      const { data } = await supabaseServerClient.from('posts')
-        .select(`*, users (
+      const { data } = await supabase.from('posts').select(`*, users (
             name, avatar
             )`);
       res.status(200).json(data);
@@ -32,7 +31,7 @@ export default async function handler(req, res) {
     case 'POST':
       const {
         data: { session },
-      } = await supabaseServerClient.auth.getSession();
+      } = await supabase.auth.getSession();
       console.log(session);
       res.status(200).json(session);
       break;
