e695d56608198673337efff4ada2fdb1c460ce0f
internetdrew <andrew@internetdrew.com>
2023-05-15 12:49:16 -0400

    successfully fetch user data from endpoint

---
+++ src/pages/api/users/[userId].js
@@ -0,0 +1,15 @@
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') return res.status(405).json('Method not allowed');
+  const { userId } = req.query;
+
+  const supabase = createServerSupabaseClient({ req, res });
+
+  const { data: userData } = await supabase
+    .from('users')
+    .select(`*`)
+    .eq('id', userId);
+
+  return res.status(200).json(userData);
+}
