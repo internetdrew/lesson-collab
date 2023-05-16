9d44fb9580608f71d37d97538e766bfb44b04e86
internetdrew <andrew@internetdrew.com>
2023-05-16 00:51:09 -0400

    fix subject selection

--- src/pages/api/posts/index.js
+++ src/pages/api/posts/index.js
@@ -1,39 +1,72 @@
 import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
 
 export default async function handler(req, res) {
-  const { method } = req;
-  const { subject } = req.query;
-  const supabase = createServerSupabaseClient({
-    req,
-    res,
-  });
+  try {
+    const { method } = req;
+    const { subject } = req.query;
+    const {
+      subject: postSubject,
+      title,
+      gradeLevel,
+      desc,
+      uid,
+      fileName,
+      fileUrl,
+    } = req.body;
+    const supabase = createServerSupabaseClient({
+      req,
+      res,
+    });
 
-  switch (method) {
-    case 'GET':
-      if (subject) {
-        const { data } = await supabase
-          .from('posts')
-          .select(
-            `*, users (
+    switch (method) {
+      case 'GET':
+        if (subject) {
+          const { data } = await supabase
+            .from('posts')
+            .select(
+              `*, users (
             name, avatar
             )`
-          )
-          .eq('subject', subject);
-        return res.status(200).json(data);
-      }
+            )
+            .eq('subject', subject);
+          return res.status(200).json(data);
+        }
 
-      const { data } = await supabase.from('posts').select(`*, users (
+        const { data } = await supabase.from('posts').select(`*, users (
             name, avatar
             )`);
-      res.status(200).json(data);
-      break;
+        res.status(200).json(data);
+        break;
 
-    case 'POST':
-      const {
-        data: { session },
-      } = await supabase.auth.getSession();
-      console.log(session);
-      res.status(200).json(session);
-      break;
+      case 'POST':
+        console.log(req.body);
+        const {
+          data: { session },
+        } = await supabase.auth.getSession();
+        console.log(session);
+
+        if (!session) {
+          return res.status(401).json({
+            error: 'not_authenticated',
+            description:
+              'The user does not have an active session or is not authenticated',
+          });
+        }
+
+        const { error } = await supabase.from('posts').insert({
+          title,
+          grade_level: gradeLevel,
+          subject: postSubject,
+          file_name: fileName,
+          file_url: fileUrl,
+          desc,
+          uid,
+        });
+        if (!error) res.status(200).json(session);
+
+        break;
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
