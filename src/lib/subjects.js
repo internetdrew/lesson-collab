68d685cf170e18992f2b5c9db229697b32640238
internetdrew <andrew@internetdrew.com>
2023-05-20 16:07:18 -0400

    add lib directory and export subjects

---
+++ src/lib/subjects.js
@@ -0,0 +1,9 @@
+const subjects = [
+  { name: 'science', current: true },
+  { name: 'math', current: false },
+  { name: 'art', current: false },
+  { name: 'social studies', current: false },
+  { name: 'history', current: false },
+];
+
+export default subjects;
