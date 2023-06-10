ad81e7ae61e00c8c27490105e34c160e811de342
internetdrew <andrew@internetdrew.com>
2023-06-10 16:39:34 -0400

    remove new user fetch from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,12 @@ import { useEffect } from 'react';
 import { Layout, Feed, SubSelector } from '../components';
 import { useSetRecoilState } from 'recoil';
 import { postsState } from '../atoms/postsAtom';
-import { newUsersState } from '../atoms/newUsersAtom';
 import { userState } from '../atoms/userAtom';
 import { useUser } from '@supabase/auth-helpers-react';
 import axios from 'axios';
 
 export default function Home({ posts, newUsers }) {
   const setPosts = useSetRecoilState(postsState);
-  const setNewUsers = useSetRecoilState(newUsersState);
   const setCurrentUser = useSetRecoilState(userState);
 
   const currentUser = useUser();
@@ -29,10 +27,6 @@ export default function Home({ posts, newUsers }) {
   useEffect(() => {
     getSetCurrentUser();
   });
-
-  useEffect(() => {
-    setNewUsers(newUsers);
-  }, [newUsers]);
 
   useEffect(() => {
     setPosts(posts);
@@ -61,11 +55,7 @@ export const getServerSideProps = async ({ req, res, query }) => {
     }`
   );
 
-  const { data: newUsers } = await axios.get(
-    `${process.env.SITE_URL}/api/users`
-  );
-
   return {
-    props: { posts, newUsers },
+    props: { posts },
   };
 };
