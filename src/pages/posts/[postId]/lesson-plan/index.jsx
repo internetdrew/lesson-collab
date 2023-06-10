3182314bf4aa31bda92c3765c6a1252f4442eb18
internetdrew <andrew@internetdrew.com>
2023-06-10 16:40:40 -0400

    fetch and render lesson plan file

--- src/pages/posts/[postId]/lesson-plan/index.jsx
+++ src/pages/posts/[postId]/lesson-plan/index.jsx
@@ -1,7 +1,30 @@
-import { PdfViewer } from '@/src/components';
+import axios from 'axios';
+import { Navbar } from '@/src/components';
 
-const index = () => {
-  return <PdfViewer />;
+const PDFView = ({ post }) => {
+  return (
+    <>
+      <Navbar />
+      <iframe
+        src={post?.file_url}
+        className='w-full h-screen relative'
+      ></iframe>
+    </>
+  );
 };
 
-export default index;
+export default PDFView;
+
+export const getServerSideProps = async ({ params }) => {
+  try {
+    const { postId } = params;
+
+    const { data: postData } = await axios.get(
+      `${process.env.SITE_URL}/api/posts/${postId}`
+    );
+
+    return { props: { post: postData[0] } };
+  } catch (error) {
+    console.error(error);
+  }
+};
