b4c9a4f103cb3642e3ec954b0328068213eaa473
internetdrew <andrew@internetdrew.com>
2023-06-18 14:20:10 -0400

    remove iframe padding

--- src/pages/posts/[postId]/lesson-plan/index.jsx
+++ src/pages/posts/[postId]/lesson-plan/index.jsx
@@ -1,24 +1,17 @@
 import axios from 'axios';
 import { Navbar } from '@/src/components';
-import { Worker } from '@react-pdf-viewer/core';
-import { Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 
 const PDFView = ({ post }) => {
   return (
     <>
       <Navbar />
-      <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js'>
-        <Viewer fileUrl={post?.file_url} />
-      </Worker>
-      {/* <div id='iframe-container'>
-        <iframe
-          src={post?.file_url}
-          allow='fullscreen'
-          credentialless
-          className='w-full h-screen'
-        />
-      </div> */}
+      <iframe
+        src={post?.file_url}
+        allow='fullscreen'
+        credentialless
+        className='w-full h-screen'
+      />
     </>
   );
 };
