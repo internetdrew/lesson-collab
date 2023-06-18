f8e4b35bfa37fa8df3f8d914fadcd92b38fd1c69
internetdrew <andrew@internetdrew.com>
2023-06-18 14:00:50 -0400

    add try react pdf viewer for mobile fixes

--- src/pages/posts/[postId]/lesson-plan/index.jsx
+++ src/pages/posts/[postId]/lesson-plan/index.jsx
@@ -1,18 +1,24 @@
 import axios from 'axios';
 import { Navbar } from '@/src/components';
+import { Worker } from '@react-pdf-viewer/core';
+import { Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
 
 const PDFView = ({ post }) => {
   return (
     <>
       <Navbar />
-      <div id='iframe-container'>
+      <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js'>
+        <Viewer fileUrl={post?.file_url} />
+      </Worker>
+      {/* <div id='iframe-container'>
         <iframe
           src={post?.file_url}
           allow='fullscreen'
           credentialless
           className='w-full h-screen'
         />
-      </div>
+      </div> */}
     </>
   );
 };
