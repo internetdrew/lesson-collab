9ab9861f585e2077de15b46e88af285eab909dca
internetdrew <andrew@internetdrew.com>
2023-06-26 10:36:10 -0400

    update favicon

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useRecoilValue } from 'recoil';
 import { NavProfile, SubjectsNav, Navbar, NewUsers } from '.';
 import { userState } from '../atoms/userAtom';
@@ -7,6 +8,21 @@ const Layout = ({ children }) => {
 
   return (
     <div className='flex min-h-full flex-col'>
+      <Head>
+        <link rel='icon' type='image/png' href='/favicon.ico' />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+      </Head>
       <Navbar />
       <div className='mx-auto flex w-full max-w-7xl items-start gap-x-8 px-4 py-28 sm:px-6 lg:px-8'>
         <aside className='sticky top-28 hidden w-72 shrink-0 md:block'>
