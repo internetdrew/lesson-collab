510a53bb0ff9376252b96498706c3a61a5ca6fdf
internetdrew <andrew@internetdrew.com>
2023-07-21 18:01:23 -0400

    optimize post loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,8 @@
-import { useEffect } from 'react';
 import { Layout, Feed, SubSelector } from '../components';
-import { useSetRecoilState } from 'recoil';
-import { postsState } from '../atoms/postsAtom';
-import { userState } from '../atoms/userAtom';
-import { useUser } from '@supabase/auth-helpers-react';
 import axios from 'axios';
 import Head from 'next/head';
 
-export default function Home({ posts }) {
-  const setPosts = useSetRecoilState(postsState);
-  const setCurrentUser = useSetRecoilState(userState);
-
-  const currentUser = useUser();
-
-  const getSetCurrentUser = async () => {
-    if (currentUser) {
-      const { data: userData } = await axios.get(
-        `/api/users/${currentUser?.id}`
-      );
-      setCurrentUser(userData[0]);
-      return;
-    }
-
-    setCurrentUser(null);
-  };
-
-  useEffect(() => {
-    getSetCurrentUser();
-  });
-
-  useEffect(() => {
-    setPosts(posts);
-  }, [posts]);
-
+export default function Home({ posts, currentUser }) {
   return (
     <>
       <Head>
@@ -72,13 +42,13 @@ export default function Home({ posts }) {
   );
 }
 
-export const getServerSideProps = async ({ req, res, query }) => {
-  try {
-    const subject = query.subject ? query.subject.toLowerCase() : null;
+export const getServerSideProps = async ({ query }) => {
+  const subject = query?.subject ? query?.subject.toLowerCase() : null;
 
+  try {
     const { data: posts } = await axios.get(
       `${process.env.SITE_URL}/api/posts${
-        subject ? `/?subject=${subject.toLowerCase()}` : ''
+        subject ? `/?subject=${subject}` : ''
       }`
     );
 
