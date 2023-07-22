0d82768e72f69cdf3a7ed88fe411206a1fa60daf
internetdrew <andrew@internetdrew.com>
2023-07-22 00:47:28 -0400

    update post keys without post id

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,8 +3,8 @@ import { LessonPost } from './';
 const Feed = ({ posts }) => {
   return (
     <section>
-      {posts?.map(post => (
-        <LessonPost key={post.id} post={post} />
+      {posts?.map((post, idx) => (
+        <LessonPost key={`post-${idx}`} post={post} />
       ))}
     </section>
   );
